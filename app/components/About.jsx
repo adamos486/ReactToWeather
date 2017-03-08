16dc24e1a756a6945fbbcd20554193f3447aef7a
adamos486 <adamos486@gmail.com>
2017-03-08 15:49:55 -0800

    Refactoring for arrow functions and stateless components.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,9 +1,9 @@
 var React = require('react');
 
-var About = React.createClass({
-  render: function() {
-    return <h3>About Component</h3>
-  }
-});
+//No state --> stateless comp.
+
+var About = (props) => {
+  return <h3>About Component</h3>
+};
 
 module.exports = About;
